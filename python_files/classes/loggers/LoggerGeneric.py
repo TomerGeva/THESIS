e036ba0a7d313dd5e56e75809ff3d7df331f6bf4
tgeva <tomerg777@gmail.com>
2021-09-28 10:16:33 +0300

    Split the logger to generic logger and VAE logger as a preperation for phase 2 logger

---
+++ python_files/classes/loggers/LoggerGeneric.py
@@ -0,0 +1,94 @@
+import os
+import math
+import datetime
+from ConfigVAE import *
+
+
+class LoggerGeneric:
+    """
+    This class holds the generic logger.
+    From this logger, the loggers for all the following phases will be built
+    """
+    def __init__(self, logdir=None, filename=None, write_to_file=True):
+        self.logdir         = logdir
+        self.filename       = filename
+        self.write_to_file  = write_to_file
+        self.verbose        = 'INFO'
+        self.header_space   = 16
+        self.result_space   = 15.6
+        self.desc_space     = 6
+        self.fileID         = None
+
+    # ==================================================================================================================
+    # Basic help functions, internal
+    # ==================================================================================================================
+    def _get_header(self):
+        temp_str = '|{0:^' + str(self.header_space) + '}| '
+        return temp_str.format(self.verbose)
+
+    # ==================================================================================================================
+    # Logging functions
+    # ==================================================================================================================
+    def start_log(self):
+        # ============================================================
+        # Starting the logging
+        # ============================================================
+        if self.filename is None:
+            # -------------------------------------
+            # Creating the filename
+            # -------------------------------------
+            self.filename = 'logger.txt'
+
+        time_data = datetime.datetime.now()
+        time_list = [time_data.day, time_data.month, time_data.year, time_data.hour, time_data.minute]
+        time_string = '_'.join([str(ii) for ii in time_list])
+        del time_data, time_list
+        if self.logdir is None:
+            self.logdir = os.path.join(os.getcwd(), time_string)
+        else:
+            self.logdir = os.path.join(self.logdir, time_string)
+
+        if self.write_to_file:
+            # -------------------------------------
+            # Creating the directory
+            # -------------------------------------
+            try:
+                os.makedirs(self.logdir)
+                print(self._get_header() +
+                      '{0:s} {1:s}'.format(' Created new directory ', self.logdir))
+            except OSError:
+                pass
+            # -------------------------------------
+            # Creating the text file log
+            # -------------------------------------
+            full_path = os.path.join(self.logdir, self.filename)
+            self.fileID = open(full_path, 'w')
+
+        elif self.verbose == 'INFO':
+            print(self._get_header() +
+                  '{0:s}'.format(' Attribute write_to_file is set to False, not writing log to file ! ! !'))
+
+        self.log_line('==============================================================================================')
+        self.log_line('==============================================================================================')
+        self.log_line('=====                                 STATING THE LOG                                    =====')
+        self.log_line('==============================================================================================')
+        self.log_line('==============================================================================================')
+
+    def end_log(self):
+        if self.write_to_file:
+            self.fileID.close()
+
+    def get_header(self, header):
+        temp_str = '{0:^' + str(self.header_space) + '}| '
+        return temp_str.format(header)
+
+    def log_line(self, line):
+        print(self._get_header() + line)
+        if self.write_to_file:
+            self.fileID.write(self._get_header() + line + '\n')
+
+    def log_title(self, title):
+        self.log_line('-------------------------------------------------------------------------------------------')
+        self.log_line('{0:^86}'.format(title))
+        self.log_line('-------------------------------------------------------------------------------------------')
+
--- python_files/classes/loggers/LoggerVAE.py
+++ python_files/classes/loggers/LoggerVAE.py
@@ -1,30 +1,19 @@
 import os
 import math
-import datetime
 from ConfigVAE import *
+from LoggerGeneric import LoggerGeneric
 
 
-class LoggerVAE:
+class LoggerVAE(LoggerGeneric):
     """
     This class holds the logger for the Variational auto-encoder
     """
     def __init__(self, logdir=None, filename=None, write_to_file=True):
-        self.logdir         = logdir
-        self.filename       = filename
-        self.write_to_file  = write_to_file
-        self.verbose        = 'INFO'
-        self.header_space   = 16
-        self.result_space   = 15.6
-        self.desc_space     = 6
-        self.fileID         = None
+        super().__init__(logdir=logdir, filename=filename, write_to_file=write_to_file)
 
     # ==================================================================================================================
     # Basic help functions, internal
     # ==================================================================================================================
-    def _get_header(self):
-        temp_str = '|{0:^' + str(self.header_space) + '}| '
-        return temp_str.format(self.verbose)
-
     def _get_result_string_train(self, wmse_loss, d_kl, cost):
         temp_str = 'W_MSE loss: {0:^' + str(self.result_space) +\
                    'f} D_kl: {1:^' + str(self.result_space) +\
@@ -104,69 +93,6 @@ class LoggerVAE:
     # ==================================================================================================================
     # Logging functions
     # ==================================================================================================================
-    def start_log(self):
-        # ============================================================
-        # Starting the logging
-        # ============================================================
-        if self.filename is None:
-            # -------------------------------------
-            # Creating the filename
-            # -------------------------------------
-            self.filename = 'logger.txt'
-
-        time_data = datetime.datetime.now()
-        time_list = [time_data.day, time_data.month, time_data.year, time_data.hour, time_data.minute]
-        time_string = '_'.join([str(ii) for ii in time_list])
-        del time_data, time_list
-        if self.logdir is None:
-            self.logdir = os.path.join(os.getcwd(), time_string)
-        else:
-            self.logdir = os.path.join(self.logdir, time_string)
-
-        if self.write_to_file:
-            # -------------------------------------
-            # Creating the directory
-            # -------------------------------------
-            try:
-                os.makedirs(self.logdir)
-                print(self._get_header() +
-                      '{0:s} {1:s}'.format(' Created new directory ', self.logdir))
-            except OSError:
-                pass
-            # -------------------------------------
-            # Creating the text file log
-            # -------------------------------------
-            full_path = os.path.join(self.logdir, self.filename)
-            self.fileID = open(full_path, 'w')
-
-        elif self.verbose == 'INFO':
-            print(self._get_header() +
-                  '{0:s}'.format(' Attribute write_to_file is set to False, not writing log to file ! ! !'))
-
-        self.log_line('==============================================================================================')
-        self.log_line('==============================================================================================')
-        self.log_line('=====                                 STATING THE LOG                                    =====')
-        self.log_line('==============================================================================================')
-        self.log_line('==============================================================================================')
-
-    def end_log(self):
-        if self.write_to_file:
-            self.fileID.close()
-
-    def get_header(self, header):
-        temp_str = '{0:^' + str(self.header_space) + '}| '
-        return temp_str.format(header)
-
-    def log_line(self, line):
-        print(self._get_header() + line)
-        if self.write_to_file:
-            self.fileID.write(self._get_header() + line + '\n')
-
-    def log_title(self, title):
-        self.log_line('-------------------------------------------------------------------------------------------')
-        self.log_line('{0:^86}' .format(title))
-        self.log_line('-------------------------------------------------------------------------------------------')
-
     def log_epoch(self, epoch_num):
         self.log_line('Epoch: {0:5d}'.format(epoch_num))
 
