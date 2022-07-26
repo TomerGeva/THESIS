Fixed all the bugs, my custom method seems to work. New signs of optimizm

common/classes/DGcnn.py
@@ -94,7 +94,7 @@ class ModDGCNN(nn.Module):
 
 class ModDGCNN2(nn.Module):
     def __init__(self, device, topology, flatten_type):
-        super(ModDGCNN, self).__init__()
+        super(ModDGCNN2, self).__init__()
         self.device          = device
         self.topology        = topology
         self.edgeconv_layers = nn.ModuleList()
@@ -113,7 +113,7 @@ class ModDGCNN2(nn.Module):
             if 'modedgeconv' in action[0]:
                 edgeconv_len += 1
                 self.edgeconv_layers.append(ModEdgeConv(action[1]))
-            if 'edgeconv' in action[0]:
+            elif 'edgeconv' in action[0]:
                 edgeconv_len += 1
                 self.edgeconv_layers.append(EdgeConv(action[1]))
             elif 'sg_pointnet' in action[0]:
@@ -139,18 +139,23 @@ class ModDGCNN2(nn.Module):
         self.conv_len = conv_len
         self.fc_len = linear_len
 
-    def forward(self, x, points):
+    def forward(self, x, points=None):
         """
-        :param x: data per point
-        :param points: coordinates
+        :param x: data per point. Size is B X D X N
+        :param points: coordinates. If None, copies the input. Size is B X F X N ; F = 2, 3 - spatial coordinates
         :return:
         """
         batch_size    = x.size(0)
+        if points is None:
+            points = x.contiguous()
         # ---------------------------------------------------------
         # passing through the edge - convolution blocks
         # ---------------------------------------------------------
         for layer in self.edgeconv_layers:
-            x = layer(points, x)
+            if type(layer) is PointNetSetAbstraction:
+                points, x = layer(points, x)
+            else:
+                x = layer(points, x)
         # ---------------------------------------------------------
         # passing through the convolution blocks
         # ---------------------------------------------------------

common/database_classes/ScatCoord_DG.py
@@ -15,7 +15,7 @@ class ScatCoordDG(Dataset):
     A sample of the dataset will be a dictionary {'grid': 2D array, 'sensitivity': target sensitivity}
     Out dataset will take additional argument 'transform' so that any required processing can be applied on the sample.
     """
-    def __init__(self, csv_files, transform=None, abs_sens=True, double_size=False):
+    def __init__(self, csv_files, transform=None, abs_sens=True, double_size=False, coord_mean=2.5, coord_scale=np.sqrt(12.5)):
         """
         Args:
         :param csv_files: logdir to the file with all the database
@@ -93,13 +93,16 @@ class ScatCoordDG(Dataset):
 # ============================================================
 class ToTensorCoord(object):
     """Convert ndarrays in sample to Tensors."""
-    def __init__(self):
+    def __init__(self, mean, scale):
         super(ToTensorCoord, self).__init__()
+        self.mean  = mean
+        self.scale = scale
 
     def __call__(self, sample):
         sensitivity, points = sample['sensitivity'], sample['coordinate_target']
         sensitivity         = torch.from_numpy(np.expand_dims(sensitivity, axis=0))
-        points              = torch.from_numpy(np.reshape(points, -1))
+        # points              = torch.from_numpy(np.reshape(points, -1))
+        points = (points - self.mean) / self.scale
         return {'sensitivity': sensitivity,
                 'coordinate_target': points}
 
@@ -107,13 +110,15 @@ class ToTensorCoord(object):
 # ======================================================================================================================
 # Defining functions which manipulate the classes above
 # ======================================================================================================================
-def import_data_sets_coord(path_list_train, path_list_test, batch_size, abs_sens=True, num_workers=1):
+def import_data_sets_coord(path_list_train, path_list_test, batch_size, abs_sens=True, coord_mean=2.5, coord_scale=np.sqrt(12.5), num_workers=1):
     """
     This function imports the train and test database
     :param path_list_train: list of paths to be used in the train dataloader
     :param path_list_test: list of paths to be used in the test dataloader
     :param batch_size: size of each batch in the databases
     :param abs_sens: if true, doing absolute value over teh sensitivity
+    :param coord_mean: normalization translation for the coordinates
+    :param coord_scale: normalization scale for the coordinates
     :param num_workers: number of workers in the dataloader
     :return: two datasets, training and test
     """
@@ -121,7 +126,7 @@ def import_data_sets_coord(path_list_train, path_list_test, batch_size, abs_sens
     # Importing complete dataset and creating train dataloader
     # --------------------------------------------------------------------------------------------------------------
     data_train = ScatCoordDG(csv_files=path_list_train,
-                             transform=None,  # ToTensorCoord(),
+                             transform=ToTensorCoord(mean=coord_mean, scale=coord_scale),
                              abs_sens=abs_sens)
     train_loader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
     # --------------------------------------------------------------------------------------------------------------
@@ -132,7 +137,7 @@ def import_data_sets_coord(path_list_train, path_list_test, batch_size, abs_sens
     for ii, dataset in enumerate(path_list_test):
         data_list = [dataset]
         temp_data = ScatCoordDG(csv_files=data_list,
-                                transform=None,  # ToTensorCoord(),
+                                transform=ToTensorCoord(mean=coord_mean, scale=coord_scale),
                                 abs_sens=abs_sens,)
         # ******************************************************************************************************
         # extracting the data-loader key from the name

common/graph_functions.py
@@ -135,11 +135,13 @@ def _square_distance(points1, points2):
     :param points2: B X N' X F
     :return: B X N X N' per-point square distance
     """
+    device      = points1.device
+
     batch_size  = points1.shape[0]
     points1_per = points1.permute(0, 2, 1)
-    distance    = torch.zeros((batch_size, points2.shape[1], points1.shape[1]))
+    distance    = torch.zeros((batch_size, points2.shape[1], points1.shape[1]), device=device)
     for ii in range(points1.shape[2]):
-        distance += (points2[:, :, ii][:, :, None] - points1_per[:, ii, :]) ** 2
+        distance += (points2[:, :, ii][:, :, None] - points1_per[:, ii, :][:, None, :]) ** 2
     return distance
 
 

common/neural_network_block_classes.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from global_const import activation_type_e, pool_e
 from global_struct import ConvBlockData, PadPoolData
 from auxiliary_functions import truncated_relu
-from graph_functions import knn
+from graph_functions import knn, sample_group
 from einops import rearrange, reduce
 from einops.layers.torch import Rearrange
 
@@ -715,7 +715,7 @@ class ModEdgeConv(nn.Module):
         if self.aggregation == 'max':
             x = x.max(dim=-1, keepdim=False)
         elif self.aggregation == 'sum':
-            x = x.sum(dim=-1, keepdim=False)
+            x = x.sum(dim=-1, keepdim=False) / self.k
         return x
 
 
@@ -762,7 +762,7 @@ class PointNetSetAbstraction(nn.Module):
         :param data: matching data for each coordinate, size is B X D X N
         :return:
             centroids: coordinates of the sampled points, size is B X F X N'
-            new_data_total: concatenation of the coordinates and the new data, size is B X (F + D) X N'
+            new_data_total: concatenation of the coordinates and the new data, size is B X D' X N'
         """
         # ==============================================================================================================
         # Permuting the input, sampling and grouping
@@ -770,7 +770,8 @@ class PointNetSetAbstraction(nn.Module):
         points = points.permute(0, 2, 1)  # B X N X F
         data   = data.permute(0, 2, 1)    # B X N X D
         if self.group_all:
-            points_new, data_grouped_total = sample_group_all(points, data)
+            pass
+            # points_new, data_grouped_total = sample_group_all(points, data)
         else:
             points_new, data_grouped_total = sample_group(self.ntag_points, self.radius, self.k, points, data)
         # ==============================================================================================================
@@ -782,11 +783,12 @@ class PointNetSetAbstraction(nn.Module):
         data_grouped_total = data_grouped_total.permute(0, 3, 2, 1)  # B X (F + D) X K X N'
         if self.residual and self.residual_layer is not None:
             res_out = F.relu(self.residual_layer_bn(self.residual_layer(data_grouped_total)))
-        for ii, (conv, bn) in zip(self.conv2d_layers, self.batch_norms):
+        for ii, conv in enumerate(self.conv2d_layers):
+            bn = self.batch_norms[ii]
             data_grouped_total = F.relu(bn(conv(data_grouped_total)))
         if self.residual:
             out_total = res_out + data_grouped_total
 
-        out_total_aggregated = torch.mean(out_total, 2)[0]
+        out_total_aggregated = torch.mean(out_total, 2)
         points_new = points_new.permute(0, 2, 1)
         return points_new, out_total_aggregated

dg_model/ConfigDG.py
@@ -11,6 +11,9 @@ from global_struct import ConvBlockData, AdaPadPoolData, PadPoolData, FCBlockDat
 XRANGE = np.array([0, 4])  # np.array([0, 19])  # Range of the x coordinate of the structure in micro-meters
 YRANGE = np.array([0, 4])  # np.array([0, 19])  # Range of the y coordinate of the structure in micro-meters
 
+COORD_MEAN  = (XRANGE[1] + XRANGE[0]) / 2
+COORD_SCALE = np.sqrt(2 * (XRANGE[1] - COORD_MEAN)**2)
+
 NUM_OF_POINTS = 500
 
 XQUANTIZE = 600  # 800  # 2500  # number of quantization points in the X coordinate
@@ -115,16 +118,16 @@ DGCNN_TOPOLOGY = [
 ]
 MODGCNN_TOPOLOGY = [
     ['modedgeconv', EdgeConvData(k=40, conv_data=ConvBlockData(in_channels=4, out_channels=64, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2), aggregation='sum')],
-    ['sg_pointnet', SetAbstractionData(ntag=NUM_OF_POINTS/2, radius=0.2, k=None, in_channel=2+64, out_channels=[64, 64], pnet_kernel=1, residual=True)],
+    ['sg_pointnet', SetAbstractionData(ntag=NUM_OF_POINTS/2, radius=0.25, k=None, in_channel=2+64, out_channels=[64, 64], pnet_kernel=1, residual=True)],
     ['modedgeconv', EdgeConvData(k=40, conv_data=ConvBlockData(in_channels=64*2, out_channels=64, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2), aggregation='sum')],
-    ['sg_pointnet', SetAbstractionData(ntag=NUM_OF_POINTS / 4, radius=0.2, k=None, in_channel=2+64, out_channels=[64, 64], pnet_kernel=1, residual=True)],
+    ['sg_pointnet', SetAbstractionData(ntag=NUM_OF_POINTS/4, radius=0.5, k=None, in_channel=2+64, out_channels=[64, 64], pnet_kernel=1, residual=True)],
     ['modedgeconv', EdgeConvData(k=40, conv_data=ConvBlockData(in_channels=64*2, out_channels=128, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2), aggregation='sum')],
-    ['sg_pointnet', SetAbstractionData(ntag=NUM_OF_POINTS / 8, radius=0.2, k=None, in_channel=2+128, out_channels=[128, 128], pnet_kernel=1, residual=True)],
+    ['sg_pointnet', SetAbstractionData(ntag=NUM_OF_POINTS/8, radius=1.0, k=None, in_channel=2+128, out_channels=[128, 128], pnet_kernel=1, residual=True)],
     ['modedgeconv', EdgeConvData(k=40, conv_data=ConvBlockData(in_channels=128*2, out_channels=256, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2), aggregation='sum')],
-    ['conv1d', ConvBlockData(768, EMBED_DIM, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2)],
+    ['conv1d', ConvBlockData(256, EMBED_DIM, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2)],
     ['linear', FCBlockData(512, in_neurons=EMBED_DIM*2, bias=False, batch_norm=True, dropout_rate=0, activation=activation_type_e.lReLU, alpha=0.2)],
-    ['linear', FCBlockData(256, in_neurons=512, bias=False, batch_norm=True, dropout_rate=0, activation=activation_type_e.lReLU, alpha=0.2)],
-    ['res-linear', ResFCBlockData(1, in_neurons=256, layers=4, bias=True, batch_norm=False, dropout_rate=0, activation=activation_type_e.null)],
+    ['res-linear', ResFCBlockData(128, in_neurons=512, layers=3, bias=True, batch_norm=False, dropout_rate=0, activation=activation_type_e.lReLU, alpha=0.2)],
+    ['linear', FCBlockData(1, in_neurons=128, bias=True, batch_norm=False, dropout_rate=0, activation=activation_type_e.null)],
 ]
 # DGCNN_TOPOLOGY = [
 #     ['edgeconv', EdgeConvData(k='all', conv_data=ConvBlockData(in_channels=4,     out_channels=16, kernel_size=1, stride=1, padding=0, bias=False, batch_norm=True, activation=activation_type_e.lReLU, alpha=0.2), aggregation='sum')],